ed01511ce699f82cb6591e4197f5a832ae79adac
Thomas AGE <thomas.age@protonmail.com>
2021-03-09 07:48:19 +0100

    Add deployer.org

--- deploy.php
+++ deploy.php
@@ -1,110 +1,36 @@
 <?php
-
-declare(strict_types=1);
-
 namespace Deployer;
 
-use Symfony\Component\Dotenv\Dotenv;
+require 'recipe/symfony.php';
 
-require 'recipe/symfony4.php';
+// Config
 
-// Project repository
-
+set('application', 'mileage-old');
+set('deploy_path', '~/{{application}}');
 set('repository', 'git@github.com:thomasage/mileage.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-
-set('git_tty', true);
-
-// Shared files/dirs between deploys
-
-set('shared_files', ['.env.local']);
+add('shared_files', []);
 add('shared_dirs', []);
-
-// Writable dirs by web server
-
 add('writable_dirs', []);
-set('allow_anonymous_stats', false);
 
 // Hosts
 
-inventory(__DIR__.'/hosts.yaml');
+host('mileage.fondcombe.eu')
+    ->set('http_user', 'fondcomb-mileage')
+    ->set('writable_mode', 'chmod')
+    ->set('bin/php', '/usr/local/php8.0/bin/php')
+    ->set('env', ['APP_ENV' => 'prod']);
 
 // Tasks
 
-desc('Execute tests');
-task(
-    'deploy:tests',
-    static function (): void {
-        runLocally('vendor/bin/simple-phpunit');
-    }
-);
-
-desc('Setup env variables');
-task(
-    'deploy:env:setup',
-    static function (): void {
-        $dotenv = new Dotenv();
-        // Remote env vars
-        run('if [ ! -f shared/.env.local ] ; then touch shared/.env.local ; fi');
-        $remoteEnvs = run('cat shared/.env.local');
-        $remoteEnvs = $dotenv->parse($remoteEnvs);
-        // Local env vars
-        $localEnvs = $dotenv->parse(file_get_contents(__DIR__.'/.env'));
-        // Populate missing env vars
-        foreach ($localEnvs as $key => $val) {
-            if (isset($remoteEnvs[$key])) {
-                continue;
-            }
-            $val = ask($key, $val);
-            if (false !== strpos($val, ' ')) {
-                $val = sprintf('"%s"', $val);
-            }
-            run(sprintf('echo "%s=%s" >> shared/.env.local', $key, str_replace('"', '\"', $val)));
-        }
-    }
-);
-
-desc('Compile assets');
 task(
     'deploy:assets',
     static function (): void {
-        runLocally('yarn run encore prod');
+        runLocally('yarn build');
         upload('public/build', '{{release_path}}/public');
     }
 );
 
-desc('Deploy project');
-task(
-    'deploy',
-    [
-        'deploy:tests',
-        'deploy:info',
-        'deploy:prepare',
-        'deploy:lock',
-        'deploy:release',
-        'deploy:update_code',
-        'deploy:shared',
-        'deploy:env:setup',
-        'deploy:vendors',
-        'deploy:assets',
-        'deploy:writable',
-        'deploy:cache:clear',
-        'deploy:cache:warmup',
-        'deploy:symlink',
-        'deploy:unlock',
-        'cleanup',
-    ]
-);
-
-// [Optional] Specific to OVH
-
-set('bin/php', '/usr/local/php7.3/bin/php');
-
-// [Optional] if deploy fails automatically unlock.
+before('deploy:writable', 'deploy:assets');
 
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'database:migrate');
