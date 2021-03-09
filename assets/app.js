77d1589bc2b30aca231de5e6d7137dfbe0cf287f
Thomas AGE <thomas.age@protonmail.com>
2021-03-09 11:18:21 +0100

    Fix jquery

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,14 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
+import 'bootstrap';
+
+$(function () {
+
+    $('tr[data-url]').on('click', (e) => {
+        window.location.href = $(e.currentTarget).data('url');
+    });
+
+});
